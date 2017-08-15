893083b56f148ba51615e9eeacbc87c0a7dd4712
“pgsoto” <dex.pgsoto@gmail.com>
2017-08-15 20:56:57 -0300

    Route::resource y el método INDEX

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CRUD Laravel
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ route('products.index') }}">Listado de productos</a>
                 </div>
             </div>
         </div>
