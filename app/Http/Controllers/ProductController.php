3a7340aa61bb63570e257c4cb7298c200f91cf4b
“pgsoto” <dex.pgsoto@gmail.com>
2017-08-15 22:40:28 -0300

    Listar y Paginar

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
     public function index(){
-        return view('products.index');
+
+        $products = Product::orderBy('id', 'ASC')->paginate();
+        return view('products.index', compact('products'));
     }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,7 +2,32 @@
 
 @section('content')
     <div class="col-sm-8">
-        Listado de productos
+        <h2>Listado de productos
+            <a href="{{ route('products.create') }}" class="btn btn-primary pull-right">Nuevo</a>
+        </h2>
+        <table class="table table-hover table-striped">
+            <thread>
+                <tr>
+                    <th width="20px">ID</th>
+                    <th>Nombre del producto</th>
+                    <th colspan="3">&nbsp;</th>
+                </tr>
+            </thread>
+            <tbody>
+            @foreach($products as $product)
+                <tr>
+                    <td>{{ $product->id }}</td>
+                    <td><strong>{{ $product->name }}</strong>
+                        {{ $product->short }}
+                    </td>
+                    <td>Ver</td>
+                    <td>Editar</td>
+                    <td>Borrar</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        {!! $products->render() !!}
     </div>
     <div class="col-sm-4">
         Mensaje
